4b8a2f94611b264676fd10b3840a21f7413fc2da
alexandr <emaxprog@gmail.com>
2016-08-18 10:39:27 +0300

    Create DB

---
+++ components/DB.php
@@ -0,0 +1,16 @@
+<?php
+
+class DB
+{
+    public static function getConnection(){
+        $paramsPath=ROOT.'/config/db_params.php';
+        $params=include $paramsPath;
+
+        $dsn="mysql:host={$params['host']};dbname={$params['dbname']}";
+        $db=new PDO($dsn,$params['user'],$params['password']);
+
+        $db->query("SET NAMES utf8");
+
+        return $db;
+    }
+}git 
---
+++ config/db_params.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+    'host'=>'testsite',
+    'dbname'=>'mysite',
+    'user'=>'root',
+    'password'=>'ghjuhfvvbcn96'
+);
